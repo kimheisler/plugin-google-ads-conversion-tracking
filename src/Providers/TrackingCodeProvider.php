d43b379cafc1ed7c7170401a8fc3ef498da6dde2
Kim <38715528+kimheisler@users.noreply.github.com>
2024-07-29 21:13:25 +0200

    Update TrackingCodeProvider.php

--- src/Providers/TrackingCodeProvider.php
+++ src/Providers/TrackingCodeProvider.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace GoogleAnalytics\Providers;
+namespace GoogleAdsConversionTracking\Providers;
 
 use Plenty\Modules\Frontend\Session\Storage\Contracts\FrontendSessionStorageFactoryContract;
 use Plenty\Plugin\Templates\Twig;
@@ -11,7 +11,7 @@ class TrackingCodeProvider
     public function call( Twig $twig )
     {
         return $twig->render(
-            'GoogleAnalytics::GoogleAnalyticsTrackingCode'
+            'GoogleAdsConversionTracking::GoogleAdsConversionTrackingTrackingCode'
         );
     }
 }
